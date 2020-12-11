5dbb817cbd2f7285a1cdc153049692b78769a0ce
mustaavaruus <rrybakov@webiomed.ai>
2020-12-11 21:07:23 +0300

    class limit

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,36 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.totalLength = 0;
+  }
+
+  _getChunkLength(chunk, encoding) {
+    return Buffer.byteLength(chunk, encoding);
+  }
+
+  _isLimitExceed() {
+    if (this.totalLength > this.limit) {
+      return true;
+    }
+    return false;
+  }
+
+  _increaseTotalLength(length) {
+    this.totalLength += length;
   }
 
   _transform(chunk, encoding, callback) {
-
+    var length = this._getChunkLength(chunk, encoding);
+    this._increaseTotalLength(length);
+    if (this._isLimitExceed()) {
+      //console.log(this.totalLength, ' ', this.limit);
+      callback(new LimitExceededError(''));
+      return;
+    }
+    this.push(chunk);
+    callback();
+    return;
   }
 }
 
