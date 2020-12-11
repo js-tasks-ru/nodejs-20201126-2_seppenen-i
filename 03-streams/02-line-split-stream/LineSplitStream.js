bf26b518ef6e1fd55fcb8bbd9b527a49137b6e3e
mustaavaruus <rrybakov@webiomed.ai>
2020-12-11 20:42:11 +0300

    class limit

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,33 +1,15 @@
 const stream = require('stream');
 const os = require('os');
-var Buffer = require('buffer/').Buffer
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
-    this.chunkBuffer = '';
-  }
-
-  _getStringFromChunk(chunk) {
-    return Buffer.from(chunk).toString();
   }
 
   _transform(chunk, encoding, callback) {
-    var processingString = this.chunkBuffer + this._getStringFromChunk(chunk);
-    var split = processingString.split(os.EOL);
-
-    for (var i = 0; i < split.length - 1; i++) {
-      //console.log(split[i]);
-      this.push(split[i]);
-    }
-    this.chunkBuffer = split[split.length - 1];
-    callback();
-
   }
 
   _flush(callback) {
-    this.push(this.chunkBuffer);
-    callback();
   }
 }
 
