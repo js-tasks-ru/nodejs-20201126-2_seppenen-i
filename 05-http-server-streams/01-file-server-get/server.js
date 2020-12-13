664315c091ccd12bfd9a2d8a8d90e9ee233c09a2
mustaavaruus <rrybakov@webiomed.ai>
2020-12-13 16:36:34 +0300

    server implementation

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,38 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+var fs = require('fs'); 
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, 'test/fixtures', pathname);
+
+  var slashSeparator = url.parse(req.url).pathname.split('/').length - 1;
+  if (slashSeparator > 1) {
+      console.log('I am here!');
+      res.statusCode = 400;
+      res.end('Folders is not implemented');
+      return;
+  }
+
+  if (!fs.existsSync(filepath)) {
+    res.statusCode = 404;
+    res.end('No such file');
+  }
 
   switch (req.method) {
     case 'GET':
+      var readStream = fs.createReadStream(filepath);
 
+      readStream.on('error', function() {
+        res.statusCode = 500;
+        res.end();
+      })
+
+      readStream.pipe(res);
       break;
 
     default:
