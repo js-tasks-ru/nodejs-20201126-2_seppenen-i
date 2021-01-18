d8e75efc745776ee5df4e621bf0d78dc091b0195
mustaavaruus <seppenen.i@yandex.ru>
2021-01-18 21:22:57 +0300

    chat implementation

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,34 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+
+var subscribeResolves = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    var message = await new Promise((resolve, reject) => {
+        subscribeResolves.push(resolve);
+    });
+
+    ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+    var message = ctx.request.body.message;
+
+    if ((message == null) || (message.length == 0)) {
+        ctx.statusCode = 400;
+        ctx.body = "body is empty"
+        return;
+    }
+
+    for (var i = 0; i < subscribeResolves.length; i++) {
+        subscribeResolves[i](message);
+    }
+
+    subscribeResolves.length = 0;
+
+    //ctx.statusCode = 200;
+    ctx.body = '';
 });
 
 app.use(router.routes());
