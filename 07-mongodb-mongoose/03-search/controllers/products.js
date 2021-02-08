3c0ed3bb9d2f4051b9ef79d84d95520cf571dadc
mustaavaruus <seppenen.i@yandex.ru>
2021-02-08 22:07:37 +0300

    3-rd task implementation

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,22 @@
+
+const Product = require("../models/Product");
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+
+  var { query } = ctx.request.query;
+
+  var products = null;
+
+  if (!query) {
+    ctx.body = { products: [] };
+  }
+  products = await Product.find(
+      { 
+        $text: { 
+          $search: query 
+        }
+      }
+    );
+  
+  ctx.body = {products};
 };
