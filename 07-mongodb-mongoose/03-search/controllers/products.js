947efb19919b674770256cdc1e306d9a50100365
mustaavaruus <seppenen.i@yandex.ru>
2021-02-08 22:44:43 +0300

    07-03 task implements

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,18 @@
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  var { query } = ctx.request.query;
+
+  var products = null;
+
+  if (!query) {
+    ctx.body = { products: [] };
+  }
+  products = await Product.find(
+      { 
+        $text: { 
+          $search: query 
+        }
+      }
+    );
+  
+  ctx.body = {products};
 };
