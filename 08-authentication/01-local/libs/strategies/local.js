4d91836166fb8cafa859581beeb92009a66bf1a3
mustaavaruus <seppenen.i@yandex.ru>
2021-02-15 20:49:54 +0300

    2 task implementation

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,25 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    {
+    	usernameField: 'email',
+    	session: false
+    },
+    async function(email, password, done) {
+
+      var user = await User.findOne({email});
+      
+      if (user == null) {
+      	done(null, false, "Нет такого пользователя");
+      }
+      
+      var isPasswordValid = await user.checkPassword(password);
+      
+      if (!isPasswordValid) {
+      	done(null, false, 'Неверный пароль');
+      }
+      
+      done(null, user);
     },
 );
