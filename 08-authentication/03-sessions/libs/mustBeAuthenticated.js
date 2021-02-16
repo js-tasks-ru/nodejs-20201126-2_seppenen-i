168c5e4e5898f991d0a070fc04ee00f6d105fb23
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-02-16 22:00:41 +0300

    Added task Работа с пользователем, сессии

--- 08-authentication/03-sessions/libs/mustBeAuthenticated.js
+++ 08-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -1,13 +1,3 @@
 module.exports = function mustBeAuthenticated(ctx, next) {
-
-  if (!ctx.user) {
-    ctx.response.status = 401;
-    ctx.body = {
-      error: 'Пользователь не залогинен',
-    };
-
-    return;
-  }
-
   return next();
 };
