f1a76008a54dc5a5a627a8e0960770825f958a5a
mustaavaruus <seppenen.i@yandex.ru>
2021-02-16 21:19:31 +0300

    03 task implementation

--- 08-authentication/03-sessions/libs/mustBeAuthenticated.js
+++ 08-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -1,3 +1,13 @@
 module.exports = function mustBeAuthenticated(ctx, next) {
+
+  if (!ctx.user) {
+    ctx.response.status = 401;
+    ctx.body = {
+      error: 'Пользователь не залогинен',
+    };
+
+    return;
+  }
+
   return next();
 };
