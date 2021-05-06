cac9b17759cc1b165f06a30f025c4f392553919d
mustaavaruus <seppenen.i@yandex.ru>
2021-05-06 13:41:16 +0300

    10 chat implementation

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,8 @@
 const Message = require('../models/Message');
+const maperMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+
+  const messagesArr = await Message.find({user: ctx.user.displayName}).sort({ $natural: -1 }).limit(20);
+  ctx.body = { messages: messagesArr.map(maperMessage)}
 };
